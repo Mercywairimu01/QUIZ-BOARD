887300cb3c7cdbc00d59b4a9be3b9b344c225f4f
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-02-06 11:49:34 +0300

    style: made the available questions an array

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 const question = document.getElementById ("question");
 const choices = Array.from(document.getElementsByClassName ("choice-text"));
-
+console.log(choices)
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -10,65 +10,75 @@ let availableQuestions = [];
 
 
 let questions =[
-  {
-     question: "When was Javascript created",
-     choice1: "2000",
-     choice2: "1995",
-     choice3: "1999",
-     choice4: "2000",
-     answer:2
-  },
-
-
-  {
-     question: "What is the type of variable data declare below const data=[ ]",
-     choice1: "String",
-     choice2: "Array",
-     choice3: "Object",
-     choice4: "None of the above",
-     answer:2
-  },
-
-
-  {
-     question: "Which of the following represents loops in Javascript",
-     choice1: "For",
-     choice2: "While",
-     choice3: "forEach",
-     choice4: "All of the above",
-     answer:1
-  },
-
-
-  {
-     question: "An object is unordered list of",
-     choice1: "Values",
-     choice2: "Names",
-     choice3: "Properties",
-     choice4: "All of the above",
-     answer:3
-  },
-  
-  
-  {
-     question: "What is the role of break keyword inside a loop",
-     choice1: "Restart the loop",
-     choice2: "Move out of the loop",
-     choice3: "Destructing in an array",
-     choice4: "None of the above",
-     answer:2
-  }
-]
-
-const correct_bonus =10;
-const max_questions = 5;
-
-
-startQuiz = ()  => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-    getNewQuestions();
-
-}
+   {
+      question: "When was Javascript created",
+      choice1: "2000",
+      choice2: "1995",
+      choice3: "1999",
+      choice4: "2000",
+      answer:2
+   },
+ 
+ 
+   {
+      question: "What is the type of variable data declare below const data=[ ]",
+      choice1: "String",
+      choice2: "Array",
+      choice3: "Object",
+      choice4: "None of the above",
+      answer:2
+   },
+ 
+ 
+   {
+      question: "Which of the following represents loops in Javascript",
+      choice1: "For",
+      choice2: "While",
+      choice3: "forEach",
+      choice4: "All of the above",
+      answer:1
+   },
+ 
+ 
+   {
+      question: "An object is unordered list of",
+      choice1: "Values",
+      choice2: "Names",
+      choice3: "Properties",
+      choice4: "All of the above",
+      answer:3
+   },
+   
+   
+   {
+      question: "What is the role of break keyword inside a loop",
+      choice1: "Restart the loop",
+      choice2: "Move out of the loop",
+      choice3: "Destructing in an array",
+      choice4: "None of the above",
+      answer:2
+   }
+ ]
+ 
+ const correct_bonus =10;
+ const max_questions = 5;
+ 
+ 
+ startQuiz = ()  => {
+     questionCounter = 0;
+     score = 0;
+     availableQuestions = [...questions];
+     console.log(availableQuestions);
+     getNewQuestion();
+ };
+ getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(math.random()= availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    choices.forEach(choice => {
+       const number = choice.dataset["number"];
+       choice.innerText = currentQuestion["choice" + number];
+    });
+ }
+availableQuestions.splice(questionIndex, 1);
